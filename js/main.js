d54a9f6dfecfcd3d247b3524ddc16dcbd9727254
Bruno Henrique <brunosh.contato@gmail.com>
2023-01-29 19:50:09 -0300

    editando valores com js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", dizOi)
+somar.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) + 1;
+})
 
-function dizOi() {
-    console.log('oi')
-    console.log("Bem-vindo ao Robotron 2000")
-}
+subtrair.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) - 1;
+})
 
-dizOi()
+
+
